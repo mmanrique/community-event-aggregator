4d3a11c0766c22ae9d518da1665400c85739d56c
Manrique <moporto@amazon.com>
2019-10-09 14:54:07 -0700

    Adding Cors origin

--- src/main/kotlin/com/mmanrique/community/event/aggregator/component/ResultsController.kt
+++ src/main/kotlin/com/mmanrique/community/event/aggregator/component/ResultsController.kt
@@ -4,6 +4,7 @@ import com.mmanrique.community.event.aggregator.CommunityAggregator
 import com.mmanrique.community.event.aggregator.model.Event
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.ResponseBody
 
@@ -14,6 +15,7 @@ class ResultsController {
     lateinit var communityAggregator: CommunityAggregator
 
     @GetMapping("/", produces = ["application/json"])
+    @CrossOrigin
     @ResponseBody
     fun returnSomething(): String {
         return "DIfferent text"
